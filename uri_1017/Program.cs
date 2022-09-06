3d7805455867538a9ceebdd2c871c9f39cc38694
LUCAS DOS SANTOS LEOCADIO <lucas.sleocadio@senacsp.edu.br>
2022-09-06 02:29:14 -0300

    Att

---
+++ uri_1017/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Globalization;
+
+namespace uri_1017
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int H = int.Parse(Console.ReadLine());
+            int KM = int.Parse(Console.ReadLine());
+
+            double QntLT = (H * KM) / 12.00;
+            Console.WriteLine(QntLT.ToString("F3", CultureInfo.InvariantCulture));
+        }
+    }
+}
